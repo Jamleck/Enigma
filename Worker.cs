bbced7290b9804309fccc412c1c7d636c2478ffc
Jamleck <contact@jamleck.com>
2020-11-27 23:38:08 +0800

    Init

--- Worker.cs
+++ Worker.cs
@@ -22,7 +22,7 @@ namespace Enigma
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(2000, stoppingToken);
             }
         }
     }
